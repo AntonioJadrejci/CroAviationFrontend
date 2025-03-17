8a43fd50e593f57c27e65e3e7d48e1b1b016fdb7
AntonioJadrejci <ajadrejcic@unipu.hr>
2025-03-17 18:22:51 +0100

    Promjenice neke za povezivanje s backendom

--- croaviation/vue.config.js
+++ croaviation/vue.config.js
@@ -1,6 +1,16 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
-})
+  ],
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // Backend server
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' }, // Ukloni /api iz URL-a
+      },
+    },
+  },
+});

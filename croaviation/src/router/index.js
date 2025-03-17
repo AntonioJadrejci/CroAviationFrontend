3ab297daa2946d2fa0e651e58f04372e9d22bbe9
AntonioJadrejci <ajadrejcic@unipu.hr>
2025-03-17 19:32:52 +0100

    Logo

--- croaviation/src/router/index.js
+++ croaviation/src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from "@/views/HomeView.vue";
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "Home",
+    component: HomeView, // Koristite HomeView umjesto HelloWorld
   },
   {
     path: '/about',

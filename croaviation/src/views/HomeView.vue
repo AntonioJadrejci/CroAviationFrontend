40380d5f8b116dccb17bf81dde654a5596c8d4e3
AntonioJadrejci <ajadrejcic@unipu.hr>
2025-03-18 17:37:12 +0100

    2 glavna buttona

--- croaviation/src/views/HomeView.vue
+++ croaviation/src/views/HomeView.vue
@@ -1,42 +1,54 @@
 <template>
-  <v-container fluid class="bg-black fill-height">
-    <v-row align="center" justify="center" class="fill-height">
-      <!-- Lijeva polovica (sadržaj) -->
-      <v-col cols="12" md="6" class="text-center">
-        <v-img
-          :src="require('../assets/CroLogo.png')"
-          class="my-3"
-          contain
-          height="200"
-          @click="goToHome"
-          style="cursor: pointer"
-        />
+  <v-app>
+    <!-- Gornja traka (App Bar) -->
+    <v-app-bar app color="black" dark>
+      <!-- Logo u gornjem lijevom kutu -->
+      <v-img
+        :src="require('../assets/CroLogo.png')"
+        contain
+        height="50"
+        class="ml-0"
+        @click="goToHome"
+        style="cursor: pointer"
+      />
 
-        <h1 class="display-2 font-weight-bold mb-3 white--text">Welcome</h1>
+      <v-spacer></v-spacer>
+      <!-- Razmak između loga i gumba -->
 
-        <p class="subheading font-weight-regular white--text">
-          For help and collaboration with other developers,
-          <br />please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-            class="white--text"
-            >Discord Community</a
-          >
-        </p>
+      <!-- Gumb za prijavu u gornjem desnom kutu -->
+      <v-btn text @click="login">
+        <v-icon left>mdi-account-circle</v-icon>
+        <!-- Ikona profila -->
+        <span>Log in</span>
+      </v-btn>
+    </v-app-bar>
 
-        <!-- Dodano za prikaz poruke s backenda -->
-        <v-alert v-if="message" type="success" class="mt-4">
-          {{ message }}
-        </v-alert>
-      </v-col>
+    <!-- Glavni sadržaj -->
+    <v-container fluid class="bg-black fill-height">
+      <v-row align="center" justify="center" class="fill-height">
+        <!-- Lijeva polovica (sadržaj) -->
+        <v-col cols="12" md="6" class="text-center">
+          <!-- Gumb "AIRPORTS" -->
+          <v-btn color="purple" x-large class="mb-4" @click="goToAirports">
+            AIRPORTS
+          </v-btn>
 
-      <!-- Desna polovica (slika) -->
-      <v-col cols="12" md="6" class="text-center">
-        <v-img :src="require('../assets/hr.png')" contain height="500" />
-      </v-col>
-    </v-row>
-  </v-container>
+          <!-- Gumb "ABOUT" -->
+          <v-btn color="purple" x-large @click="goToAbout"> ABOUT </v-btn>
+
+          <!-- Dodano za prikaz poruke s backenda -->
+          <v-alert v-if="message" type="success" class="mt-4">
+            {{ message }}
+          </v-alert>
+        </v-col>
+
+        <!-- Desna polovica (slika) -->
+        <v-col cols="12" md="6" class="text-center">
+          <v-img :src="require('../assets/hr.png')" contain height="500" />
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
@@ -59,6 +71,24 @@ export default {
         // Ako je korisnik već na homepageu, osvježi stranicu
         window.location.reload();
       }
+    },
+
+    // Metoda za prijavu
+    login() {
+      // Ovdje možete dodati logiku za prijavu (npr. preusmjeravanje na stranicu za prijavu)
+      console.log("Kliknuto na Log in");
+    },
+
+    // Metoda za navigaciju na stranicu "AIRPORTS"
+    goToAirports() {
+      console.log("Kliknuto na AIRPORTS");
+      // Ovdje možete dodati logiku za navigaciju na stranicu "AIRPORTS"
+    },
+
+    // Metoda za navigaciju na stranicu "ABOUT"
+    goToAbout() {
+      console.log("Kliknuto na ABOUT");
+      // Ovdje možete dodati logiku za navigaciju na stranicu "ABOUT"
     },
   },
 
